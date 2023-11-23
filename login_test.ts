c36d693d6aa19305d497dbc2def46b9ac3565bab
Joe Duong <chau.duong1995@gmail.com>
2023-11-23 13:24:13 -0800

    login_test_update

--- login_test.ts
+++ login_test.ts
@@ -1,7 +1,15 @@
 Feature('Login Test');
 
+const username:string = 'texternalauth@iserviceauto.com';
+const password:string = 'test';
+
 Scenario('test something', ({ I }) => {
   I.amOnPage('https://member.staging.iserviceauto.com/');
   I.see('iService');
+
+  //I.seeElement({name: 'username'});
+  I.fillField('username', username);
+  I.fillField('password', secret(password));
+  I.click('submit', '#div.d-flex.justify-content-end');
   pause(); 
 });
